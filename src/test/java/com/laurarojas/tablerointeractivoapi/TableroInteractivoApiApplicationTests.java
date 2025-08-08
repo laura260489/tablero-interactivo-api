b4354c470947d1faa9460aff632ccadf2781c45d
laura260489 <lauradiezmil@hotmail.com>
2025-08-08 10:57:25 -0500

    Add initial application spring boot

---
+++ src/test/java/com/laurarojas/tablerointeractivoapi/TableroInteractivoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurarojas.tablerointeractivoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableroInteractivoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
